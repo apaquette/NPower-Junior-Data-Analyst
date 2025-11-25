56f19c95ed394463ae5f1208f325610f0eb67bc6
Alex Paquette <alexandre.d.paquette@gmail.com>
2025-11-25 12:36:21 -0400

    update shell to use VS Code with FHS

--- python-shell.nix
+++ python-shell.nix
@@ -2,25 +2,34 @@
 
 pkgs.mkShell {
   name = "python-env";
-  buildInputs = [
+  packages = [
     pkgs.fish
     pkgs.git
-    # VS Code with extensions
-    (pkgs.vscode-with-extensions.override {
-      vscodeExtensions = with pkgs.vscode-extensions; [
-        ms-python.python
-        ms-python.debugpy
-        ms-toolsai.jupyter
-        ms-python.vscode-pylance
-        ms-python.black-formatter
-        ms-python.flake8
-        njpwerner.autodocstring
-        almenon.arepl
 
-        pkief.material-icon-theme
-        bbenoist.nix
-      ];
-    })
+    # VS Code FHS
+    (pkgs.vscode.fhsWithPackages (ps: with ps; [
+      python3
+      jupyter
+      
+      nix
+    ]))
+
+    # VS Code with extensions
+    # (pkgs.vscode-with-extensions.override {
+    #   vscodeExtensions = with pkgs.vscode-extensions; [
+    #     ms-python.python
+    #     ms-python.debugpy
+    #     ms-toolsai.jupyter
+    #     ms-python.flake8
+    #     # ms-python.vscode-pylance
+    #     # ms-python.black-formatter
+    #     # njpwerner.autodocstring
+    #     # almenon.arepl
+
+    #     pkief.material-icon-theme
+    #     bbenoist.nix
+    #   ];
+    # })
 
     # Python environment
     (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
@@ -35,7 +44,4 @@ pkgs.mkShell {
       debugpy
     ]))
   ];
-  shellHook =''
-    exec fish
-  '';
 }
