9fa7d0c2fe3d456fe0e870b7fd095f4d420cca16
Alex Paquette <alexandre.d.paquette@gmail.com>
2025-11-26 16:47:42 -0400

    implement simple program to calculate time remaining until deadline

---
+++ review/Python Tutorial for Beginners/my-python-project/time_till_deadline.py
@@ -0,0 +1,15 @@
+"""_summary_
+"""
+
+from datetime import datetime
+
+user_input = input("enter your goal with a deadline separated by colon\n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline_date = datetime.strptime(input_list[1], "%d.%m.%Y")
+today_date = datetime.today()
+
+time_remaining = deadline_date - today_date
+
+print(f"Time remaining for your goal: {goal} is {time_remaining.days} days")
