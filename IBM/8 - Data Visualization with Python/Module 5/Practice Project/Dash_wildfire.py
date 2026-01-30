ff32afdf16dc0af68220463180d0ed40b1e7a03b
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-01-30 17:51:50 -0400

    Complete course 8 practice project

---
+++ IBM/8 - Data Visualization with Python/Module 5/Practice Project/Dash_wildfire.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output, State
+import plotly.graph_objects as go
+import plotly.express as px
+from dash import no_update
+import datetime as dt
+
+app = dash.Dash(__name__)
+
+#app.config.suppress_callback_exceptions = True
+
+df = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/Historical_Wildfires.csv')
+
+df['Month'] = pd.to_datetime(df['Date']).dt.month_name()
+df['Year'] = pd.to_datetime(df['Date']).dt.year
+
+html_Title = html.H1('Australia Wildfire Dashboard',
+                     style={'textAlign':'center', 'color': '#503D36', 'font-size':26})
+
+radio_regions = html.Div([
+    html.H2('Select Region: ', style={'margin-right':'2em'}),
+    dcc.RadioItems(['NSW','QL','SA','TA','VI','WA'], 'NSW', id='region', inline=True)
+])
+
+dropdown_Year = html.Div([
+    html.H2('Select Year:', style={'margin-right':'2em'}),
+    dcc.Dropdown(df.Year.unique(), value=2005, id='year')
+])
+
+app.layout = html.Div(children=[
+    html_Title,
+    radio_regions,
+    dropdown_Year,
+    html.Div([
+        html.Div([],id='plot1'),
+        html.Div([],id='plot2')
+    ], style={'display':'flex'})
+])
+
+@app.callback([
+    Output(component_id='plot1', component_property='children'),
+    Output(component_id='plot2', component_property='children')],
+[
+    Input(component_id='region', component_property='value'),
+    Input(component_id='year', component_property='value')])
+def reg_year_display(region, year):
+    region_data = df[df['Region']==region]
+    y_r_data = region_data[region_data['Year']==year]
+
+    est_data = y_r_data.groupby('Month')['Estimated_fire_area'].mean().reset_index()
+    fig1 = px.pie(est_data, values='Estimated_fire_area', names='Month', title=f'{region} : Monthly Average Estimated Fire Area in year {year}')
+
+    veg_data = y_r_data.groupby('Month')['Count'].mean().reset_index()
+    fig2 = px.bar(veg_data, x='Month', y='Count', title=f'{region} : Average Count of Pixels for Presumed Vegetation Fires in year {year}')
+
+    return [dcc.Graph(figure=fig1), dcc.Graph(figure=fig2)]
+
+if __name__ == '__main__':
+    app.run()
